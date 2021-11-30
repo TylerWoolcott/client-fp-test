9a1bc5fefea9e288a02f6482d9e7b41068b4b91c
Tyler Woolcott <Tyler.woolcott@gmail.com>
2021-11-30 21:36:12 +0000

    update json contracts

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import OpenAR from './utils/OpenAR.json'
 
 // const OpenARAddress = "0xb43F4A509B296150E63eEe50a96bd96cD7CdD084" //localhost:8545
 
-const OpenARAddress = "0xdF41435F04AfF150e247ee3DFE55f25E03cc9ED0" // Ropsten
+const OpenARAddress = "0x428A01748BBFfBaeD60720e43640b4618F2fa0df" // Ropsten
 
 function App() {
   const [currentAccount, setCurrentAccount] = useState("");
